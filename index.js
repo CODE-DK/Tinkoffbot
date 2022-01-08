cfd3cff12cbeb4ca26754d6670b991a280b81a3a
DK <>
2022-01-08 22:16:56 +0500

    Add first figi to project

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const OpenAPI = require('@tinkoff/invest-openapi-js-sdk');
+import OpenAPI from '@tinkoff/invest-openapi-js-sdk';
+import dotenv from 'dotenv';
 
-const dotenv = require('dotenv');
 dotenv.config();
 
-const apiURL = process.env.URL;
-const socketURL = process.env.URL_SOCKET;
-const secretToken = process.env.TOKEN;
+const api = new OpenAPI({ 
+    apiURL: process.env.URL,
+    socketURL: process.env.URL_SOCKET,
+    secretToken: process.env.TOKEN
+});
 
-const api = OpenAPI({ apiURL, socketURL, secretToken });
-
-//TODO: Here we will write our bot
-
-function main() {}
+async function main() {
+	try {
+		const { figi, name } = await api.searchOne({ figi: 'BBG004731489' });
+        console.log(`Found figi=${figi} for name=${name}`);
+        const candales = await api.candlesGet({
+            figi,
+            from: '2019-08-19T18:38:33.131642+03:00',
+            to: '2019-08-19T18:48:33.131642+03:00', 
+            interval: '1min'
+        });
+		console.log(candales);
+	} catch (e) {
+		console.log(e);
+	}
+}
 main();
